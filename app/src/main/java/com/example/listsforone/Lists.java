a320bc775f2d1f58380db8b63d061f19eae8600c
OMGSAMUELRBR <samueldynamo11@gmail.com>
2023-02-06 21:16:33 +0100

    Update

--- app/src/main/java/com/example/listsforone/Lists.java
+++ app/src/main/java/com/example/listsforone/Lists.java
@@ -10,5 +10,6 @@ public class Lists extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists);
+        System.out.println("Hola");
     }
 }
