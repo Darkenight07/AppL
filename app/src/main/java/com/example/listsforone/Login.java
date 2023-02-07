1b507bc8a3faaf5c7c2fc27a05d6d33a306a21e0
OMGSAMUELRBR <samueldynamo11@gmail.com>
2023-02-07 21:20:08 +0100

    Update

--- app/src/main/java/com/example/listsforone/Login.java
+++ app/src/main/java/com/example/listsforone/Login.java
@@ -3,8 +3,13 @@ package com.example.listsforone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -34,6 +39,24 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(Login.this, "Iniciando sesion", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(Login.this, Lists.class));
                 } else {
+
+                    Animation shake = AnimationUtils.loadAnimation(Login.this, R.anim.shake);
+
+                    float red = 1.2f;
+                    float green = 1.0f;
+                    float blue = 1.0f;
+                    float alpha = 1.0f;
+
+                    ColorMatrix matrix = new ColorMatrix();
+                    matrix.setScale(red, green, blue, alpha);
+                    ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+                    // user.setBackgroundColor(Color.RED);
+                    user.getBackground().setColorFilter(filter);
+                    password.getBackground().setColorFilter(filter);
+
+                    password.startAnimation(shake);
+                    user.startAnimation(shake);
+
                     Toast.makeText(Login.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -42,4 +65,5 @@ public class Login extends AppCompatActivity {
     public void Registrar(View view) {
         startActivity(new Intent(Login.this, Register.class));
     }
+
 }
