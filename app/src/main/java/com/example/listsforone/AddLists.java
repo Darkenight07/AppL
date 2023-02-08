ebf1691df10472a5de0b5abfefecbc4db9dbcc2b
OMGSAMUELRBR <samueldynamo11@gmail.com>
2023-02-08 21:44:57 +0100

    Update

--- app/src/main/java/com/example/listsforone/AddLists.java
+++ app/src/main/java/com/example/listsforone/AddLists.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddLists extends AppCompatActivity {
     EditText AddListNameID;
     Button ButtonAddListsID;
+    Button BackButtonListsID;
 
     @SuppressLint("MissingInflatedId")
 
@@ -22,16 +24,29 @@ public class AddLists extends AppCompatActivity {
 
         AddListNameID = findViewById(R.id.AddListNameID);
         ButtonAddListsID = findViewById(R.id.ButtonAddListsID);
+        BackButtonListsID = findViewById(R.id.BackButtonListsID);
 
         ButtonAddListsID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text_ListName = AddListNameID.getText().toString();
 
-                System.out.println(text_ListName);
+                Toast.makeText(AddLists.this, "Agregando " + text_ListName, Toast.LENGTH_SHORT).show();
+
+                Intent sendVariableListName = new Intent(AddLists.this, Lists.class);
+                sendVariableListName.putExtra("text_ListName", text_ListName);
+                startActivity(sendVariableListName);
 
                 startActivity(new Intent(AddLists.this, Lists.class));
             }
+        });
+
+        BackButtonListsID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AddLists.this, Lists.class));
+            }
+
         });
     }
 }
--- app/src/main/java/com/example/listsforone/Lists.java
+++ app/src/main/java/com/example/listsforone/Lists.java
@@ -2,14 +2,33 @@ package com.example.listsforone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Lists extends AppCompatActivity {
 
+    Button addListsButtonID;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists);
+
+        addListsButtonID = findViewById(R.id.ButtonAddListsID);
+
+        // Pasando la variable text_ListName de la clase AddLists.java a este
+
+        Intent sendVariableListName = getIntent();
+        String text_ListName = sendVariableListName.getStringExtra("text_ListName");
+
+        System.out.println(text_ListName);
+
+    }
+    public void addListButton(View view) {
+        startActivity(new Intent(Lists.this, AddLists.class));
 
     }
 }
