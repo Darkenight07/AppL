51e3bbe3b9ee506a10c47ac8ae8f68172aee4270
OMGSAMUELRBR <samueldynamo11@gmail.com>
2023-02-08 22:53:17 +0100

    Update

--- app/src/main/java/com/example/listsforone/Lists.java
+++ app/src/main/java/com/example/listsforone/Lists.java
@@ -3,32 +3,74 @@ package com.example.listsforone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Lists extends AppCompatActivity {
 
-    Button addListsButtonID;
-    @SuppressLint("MissingInflatedId")
+    private Button buttonID;
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists);
 
-        addListsButtonID = findViewById(R.id.ButtonAddListsID);
+        buttonID = findViewById(R.id.addListsButtonID);
+        listView = findViewById(R.id.ListView);
 
-        // Pasando la variable text_ListName de la clase AddLists.java a este
+        buttonID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
 
-        Intent sendVariableListName = getIntent();
-        String text_ListName = sendVariableListName.getStringExtra("text_ListName");
-
-        System.out.println(text_ListName);
-
+            }
+        });
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
     }
-    public void addListButton(View view) {
-        startActivity(new Intent(Lists.this, AddLists.class));
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Items eliminados", Toast.LENGTH_LONG).show();
+
+                items.remove(i);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+
+            }
+        });
+    }
+
+    private void addItem(View view) {
+        EditText input = findViewById(R.id.text_ListName);
+        String itemText = input.getText().toString();
+
+
+        if(!(itemText.equals(""))) {
+            itemsAdapter.add(itemText);
+            input.setText("");
+        } else {
+            Toast.makeText(getApplicationContext(), "Por favor inserte texto", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
